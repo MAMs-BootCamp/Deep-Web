d6783314164e5ceeca1a90003a020af9d23ae73c
mohmmed23 <mhmmedben3@gmail.com>
2022-07-29 16:24:26 +0300

    create add item to array fun #3

--- logic.js
+++ logic.js
@@ -1,21 +1,32 @@
 function creatItem(name, price, category, img, amount) {
-    if (name && price && category && img && amount) {
-        if (typeof (name) === 'string' && typeof (price) === 'number' && typeof (category) === 'string' && typeof (img) === 'string' && typeof (amount) === 'number') {
-            let item = {
-                name: name,
-                price: price,
-                category: category,
-                img: img,
-                amount: amount,
-            }
-            return item;
-
-        } else if (typeof (price) !== 'number' || typeof (amount) !== 'number') {
-            return 'This is not a NUMBER!'
-        } else { return 'This is not a STRING!' }
+  if (name && price && category && img && amount) {
+    if (
+      typeof name === 'string' &&
+      typeof price === 'number' &&
+      typeof category === 'string' &&
+      typeof img === 'string' &&
+      typeof amount === 'number'
+    ) {
+      let item = {
+        name: name,
+        price: price,
+        category: category,
+        img: img,
+        amount: amount,
+      }
+      return item
+    } else if (typeof price !== 'number' || typeof amount !== 'number') {
+      return 'This is not a NUMBER!'
     } else {
-        return 'Complete the form!'
+      return 'This is not a STRING!'
     }
+  } else {
+    return 'Complete the form!'
+  }
+}
+function addItem(array, item) {
+  array.push(item)
+  return array
 }
 
-module.exports = { creatItem }
+module.exports = { creatItem, addItem }
