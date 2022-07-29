75dac2d976f512984be96f9596f1c83b9037e415
mohammadalwadia@gmail.com <54046997+mwadia@users.noreply.github.com>
2022-07-29 17:44:36 +0300

    Build all pure functions #2

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-const { creatItem, addItem, search } = require('../logic')
+const { creatItem, addItem, search, editItem, removeItem, sortingArray, filterCategory } = require('../logic')
 
 test('creat new item', () => {
   expect(creatItem('Kakashi', 20, 'Anime', 'image', 1)).toEqual({
@@ -67,4 +67,100 @@ test('Item search ', () => {
     { name: 'kakashi' },
     { name: 'Kaka' },
   ])
+})
+
+test('edit item', () => {
+  expect(editItem(
+    {
+      name: 'Kakashi',
+      price: 20,
+      category: 'Anime',
+      img: 'image',
+      amount: 1,
+    },
+    'mohammad',
+    '',
+    '',
+    '',
+    '',
+  )).toEqual({
+    name: 'mohammad',
+    price: 20,
+    category: 'Anime',
+    img: 'image',
+    amount: 1,
+  })
+})
+
+test('edit item', () => {
+  expect(editItem(
+    {
+      name: 'Kakashi',
+      price: 20,
+      category: 'Anime',
+      img: 'image',
+      amount: 1,
+    },
+    'mohammad',
+    40,
+    '',
+    '',
+    '',
+  )).toEqual({
+    name: 'mohammad',
+    price: 40,
+    category: 'Anime',
+    img: 'image',
+    amount: 1,
+  })
+})
+
+test('edit item', () => {
+  expect(editItem(
+    {
+      name: 'Kakashi',
+      price: 20,
+      category: 'Anime',
+      img: 'image',
+      amount: 1,
+    },
+    'mohammad',
+    '40',
+    '',
+    '',
+    '',
+  )).toEqual({
+    name: 'mohammad',
+    price: 20,
+    category: 'Anime',
+    img: 'image',
+    amount: 1,
+  })
+})
+
+test('remove an item from array ', () => {
+  expect(removeItem([{ id: 1 }, { id: 2 }], { id: 1 })).toEqual([
+    { id: 2 },
+  ])
+})
+test('remove an item from array ', () => {
+  expect(removeItem([{ id: 1 }, { id: 2 }], {})).toEqual([
+    { id: 1 }, { id: 2 }])
+})
+
+test('sorting  the array ', () => {
+  expect(sortingArray([{ price: 20 }, { price: 40 }, { price: 30 }])).toEqual([
+    { price: 20 }, { price: 30 }, { price: 40 }])
+})
+test('sorting  the array ', () => {
+  expect(sortingArray([{ price: 20 }])).toEqual([
+    { price: 20 }])
+})
+test('sorting  the array ', () => {
+  expect(sortingArray([])).toEqual([])
+})
+
+test('filter the array by category', () => {
+  expect(filterCategory([{ category: 'mobiles' }, { category: 'labtops' }], 'mobiles'))
+    .toEqual([{ category: 'mobiles' }])
 })
